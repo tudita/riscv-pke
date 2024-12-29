58f7017038a51bfc11caf9f25f380a1e0a271fff
tudita <1282394825@qq.com>
2024-12-29 17:17:33 +0000

    tmp

--- kernel/rfs.c
+++ kernel/rfs.c
@@ -494,14 +494,7 @@ struct vinode *rfs_create(struct vinode *parent, struct dentry *sub_dentry) {
   // nlinks, i.e., the number of links.
   // blocks, i.e., its block count.
   // Note: DO NOT DELETE CODE BELOW PANIC.
-
-  free_dinode->size = 0;
-  free_dinode->type = R_FILE;
-  free_dinode->nlinks = 1;
-  free_dinode->blocks = 1;
-
-
-  //panic("You need to implement the code of populating a disk inode in lab4_1.\n" );
+  panic("You need to implement the code of populating a disk inode in lab4_1.\n" );
 
   // DO NOT REMOVE ANY CODE BELOW.
   // allocate a free block for the file
@@ -598,7 +591,19 @@ int rfs_link(struct vinode *parent, struct dentry *sub_dentry, struct vinode *li
   //    rfs_add_direntry here.
   // 3) persistent the changes to disk. you can use rfs_write_back_vinode here.
   //
-  panic("You need to implement the code for creating a hard link in lab4_3.\n" );
+  link_node->nlinks++;
+  int ret = rfs_add_direntry(parent, sub_dentry->name, link_node->inum);
+  if(ret == -1) {
+    sprint("rfs_link: rfs_add_direntry failed\n");
+    return -1;
+  }
+  ret = rfs_write_back_vinode(parent);
+  if(ret == -1) {
+    sprint("rfs_link: rfs_write_back_vinode failed\n");
+    return -1;
+  }
+  return 0;
+  //panic("You need to implement the code for creating a hard link in lab4_3.\n" );
 }
 
 //
@@ -794,10 +799,7 @@ int rfs_readdir(struct vinode *dir_vinode, struct dir *dir, int *offset) {
   // the method of returning is to popular proper members of "dir", more specifically,
   // dir->name and dir->inum.
   // note: DO NOT DELETE CODE BELOW PANIC.
-  dir->inum = p_direntry->inum;
-  strcpy(dir->name, p_direntry->name);
-
-  //panic("You need to implement the code for reading a directory entry of rfs in lab4_2.\n" );
+  panic("You need to implement the code for reading a directory entry of rfs in lab4_2.\n" );
 
   // DO NOT DELETE CODE BELOW.
   (*offset)++;
